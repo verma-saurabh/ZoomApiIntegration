e4937fa57047f9d37721760e23d4b797a979d275
Saurabh <saurabh.verma.miet@gmail.com>
2019-12-23 10:31:02 +0530

    Added Zoom button

--- src/main/java/com/integration/ZoomApiIntegration/Constants/Constants.java
+++ src/main/java/com/integration/ZoomApiIntegration/Constants/Constants.java
@@ -3,7 +3,7 @@ package com.integration.ZoomApiIntegration.Constants;
 public class Constants {
     public static final String clientId = "4_2R08mRkGyK8_NQFU_Vg";
     public static final String clientSecret = "RKpDuWBrkBtPPjTwBeJ9pa7JR30uLVHf";
-    public static final String redirect_uri = "https://d6b4a645.ngrok.io/User/auth";
+    public static final String redirect_uri = "https://aa628c6d.ngrok.io/User/auth";
 
     static public class Zoom {
         public static final String zoomApiBaseUrl = "https://api.zoom.us/v2";
